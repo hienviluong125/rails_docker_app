3ffe4f41016b1a0279403745a290ba6ea9208876
hienviluong125 <hienviluong125@gmail.com>
2021-06-03 20:42:25 +0700

    define CI/CD to redeploy ECS
    update root API to test CI/CD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    render json: { success: true, message: "Hello Rails Docker App v1.0.1" }
+    render json: { success: true, message: "Hello Rails Docker App Trigger In Master Branch" }
   end
 end
